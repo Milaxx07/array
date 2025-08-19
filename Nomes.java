563699535d5303fd256d5bb02a10bcba47b1fffb
Milaxx07 <camilabomfim2007@gmail.com>
2025-08-19 19:08:03 -0300

    Add files via upload
    Acrescente seu nome na posição 3 do vetor e exiba-o em tela

---
+++ Nomes.java
@@ -0,0 +1,9 @@
+//Acrescente seu nome na posição 3 do vetor e exiba-o em tela
+public class Nomes{
+public static void main(String[] args) {
+        String nomes[] = new String [3];
+        nomes[3] = "Camila";
+        System.out.println(nomes[3]);
+    }
+
+}
